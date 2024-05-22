3bd27fcee38bc5ab9e0cda1bbbce060847b04bad
Claudia Lamas <claudia.c.lamas@gmail.com>
2024-05-22 15:34:52 +0100

    server ok. chat sending messages between players

---
+++ cluedoGame/src/messages/ServerMessages.java
@@ -0,0 +1,7 @@
+package messages;
+
+public class ServerMessages {
+    public static final String SERVER_STARTED = "Server started on port: %s";
+    public static final String SERVER_COULNT_LAUNCH = "Server could not launch due to IOException";
+    public static final String SERVER_ERROR = "Something went wrong with the server. Connection closing...";
+}
